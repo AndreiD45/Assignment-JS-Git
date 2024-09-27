1989de0496e601f745719449dcaca3de4234b78f
Dolean Andrei <forandrei@yahoo.com>
2024-09-27 11:02:30 +0300

    did excercise 1

---
+++ exercise-01/main.js
@@ -0,0 +1,7 @@
+let distanceOne = prompt("Input a value of kilometers to be converted to miles: ");
+distanceOne = Number(distanceOne);
+
+let distanceTwo = distanceOne * 0.62;
+distanceTwo = Number(distanceTwo);
+
+alert(`${distanceOne} Kilometers = ${distanceTwo} Miles`);
